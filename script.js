4b551ac8a78f8a613c14e5fc241253cab0b5001b
Pelicabs <fongmarcus2005@gmail.com>
2022-12-10 15:46:12 +0800

    adding env (you cannot see it)

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
+require('dotenv').config()
+const { MongoClient } = require("mongodb");
 const express = require("express")
 
 const app = express()
+const client = new MongoClient(process.env.MONGO_URI)
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+async function run() {
+    try {
+        const database = client.db('authentication');
+        const users = database.collection('users');
+        // await users.insertOne({
+        //     username: 'tim',
+        //     password: 'verydifficulttoguess'
+        // })
 
+        const user = await users.findOne({ username: 'tim' })
+        console.log('found user', user)
+        
+        app.use(express.urlencoded({ extended: true }));
+        app.use(express.json());
 
-// req = request, res = response
-app.get('/', (req, res) => {
-    res.send("hi")
-})
+        // req = request, res = response
+        app.get('/', (req, res) => {
+            res.send("hello there")
+        })
 
-app.listen(3000, () => {
-    console.log('server started on port 3000')
-})
+        app.listen(3000, () => {
+            console.log('server started on port 3000')
+        })
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+  }
+
+  run().catch(console.dir);
