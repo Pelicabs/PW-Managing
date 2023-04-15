f5bc21a343144a83cf6bcdd69b77d099167b0933
Pelicabs <fongmarcus2005@gmail.com>
2023-04-15 14:44:36 +0800

    Adding functionality to login button, made plans

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,15 +1,41 @@
+import React from 'react'
 import { Button, Checkbox, Form } from 'semantic-ui-react'
+import axios from 'axios'
 
 function LoginForm() {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    async function onLogin() {
+        try {
+            const response = await axios.post('/api/auth/login', {
+                email: email,
+                password: password
+            })
+            console.log(response)
+            } 
+        catch (error) {
+            console.log(error)
+        }
+    }
+    
     return (
-        <Form>
+        <Form onSubmit={onLogin}>
             <Form.Field>
-                <label>Email</label>
-                <input placeholder='Email' />
+            <label>Email</label>
+                <input
+                    placeholder='Email'
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
             </Form.Field>
             <Form.Field>
-                <label>Password</label>
-                <input placeholder='Password' />
+            <label>Password</label>
+                <input
+                    placeholder='Password'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
             </Form.Field>
             <Form.Field>
                 <Checkbox label='I agree to Marcus Terms and Conditions' />
